1cd8c26ca506c37fd92139d0a1fdfd9bc0433172
ParvesPR <parves2585@gmail.com>
2024-02-23 23:49:37 +0800

    find even number using for loop

--- forloop.js
+++ forloop.js
@@ -7,3 +7,9 @@ for (var x = 0; x <= 100; x++) {
         console.log(x);
     }
 }
+//PRINT EVEN NUMBER BETWEEN 1 TO 100
+for (var x = 0; x <= 100; x++) {
+    if (x % 2 === 0) {
+        console.log(x);
+    }
+}
