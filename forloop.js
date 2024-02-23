e089552a48d6fa70a6c7e09b5bc18ea0aeb70809
ParvesPR <parves2585@gmail.com>
2024-02-23 23:42:14 +0800

    find odd number using for loop

---
+++ forloop.js
@@ -0,0 +1,9 @@
+for (var x = 0; x < 100; x++) {
+    console.log(x + 1, 'Parves');
+}
+//PRINT ODD NUMBER BETWEEN 1 TO 100
+for (var x = 0; x <= 100; x++) {
+    if (x % 2 === 1) {
+        console.log(x);
+    }
+}
