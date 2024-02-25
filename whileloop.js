1dafa228258240bdcaba5dd4cd4261668ba6c898
ParvesPR <parves2585@gmail.com>
2024-02-25 12:55:14 +0800

    conditionaly match number using while loop

--- whileloop.js
+++ whileloop.js
@@ -1,5 +1,17 @@
 var x = 0;
 while (x < 10) {
-    console.log('Parves');
+    // console.log('Parves');
     x++
+}
+// RANDOM NUMBER GENERATE AND MATCH
+var isRunning = true;
+while (isRunning) {
+    var ran = (Math.round(Math.random() * 10 + 1));
+    if (ran === 9) {
+        console.log('Got ' + ran + ' & Random number match');
+        isRunning = false;
+    }
+    else {
+        console.log('Got ' + ran + ' and did not match');
+    }
 }
