a5218340b0cfe8ffeaf6babd5c940bd16d7e51af
ParvesPR <parves2585@gmail.com>
2024-02-26 21:15:33 +0800

    example of continue statement

--- continue.js
+++ continue.js
@@ -6,3 +6,11 @@ for (var x = 0; x <= 10; x++) {
         console.log(x);
     }
 }
+for (var x = 0; x <= 10; x++) {
+    if (x % 2 === 0 || x === 4) {
+        continue; /*When the condition is matched It will continue   */
+    }
+    else {
+        console.log(x);
+    }
+}
