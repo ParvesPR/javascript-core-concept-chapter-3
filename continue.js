65409c4d123fa95b123f23538f465c14155597f2
ParvesPR <parves2585@gmail.com>
2024-02-26 20:44:49 +0800

    continue statement

---
+++ continue.js
@@ -0,0 +1,8 @@
+for (var x = 0; x <= 10; x++) {
+    if (x === 2 || x === 4) {
+        continue; /*When the condition is matched It will continue   */
+    }
+    else {
+        console.log(x);
+    }
+}
